26d0d70fb8238a5852e301d12e113546d82eec87
linnsan2000 <31280120+linnsan2000@users.noreply.github.com>
2018-01-31 09:25:06 +0100

    BArchart

--- frontend/src/components/statusView/index.js
+++ frontend/src/components/statusView/index.js
@@ -18,21 +18,20 @@ export default class StatusView extends React.Component {
     return (
       <div className="modal-backdrop">
         <div className="modal">
-          <p>Vänsterpartiet: {this.props.status.Vänsterpartiet}</p>
-          <p>Socialdemokraterna: {this.props.status.Socialdemokraterna}</p>
-          <p>Miljöpartiet: {this.props.status.Miljöpartiet}</p>
-          <p>Centerpartiet: {this.props.status.Centerpartiet}</p>
-          <p>Sverigedemokraterna: {this.props.status.Sverigedemokraterna}</p>
-          <p>Moderaterna: {this.props.status.Moderaterna}</p>
-          <p>Kristdemokraterna: {this.props.status.Kristdemokraterna}</p>
-          <p>Liberalerna: {this.props.status.Liberalerna}</p>
+          <p>Vänsterpartiet {this.props.status.Vänsterpartiet}</p>
+          <p>Socialdemokraterna {this.props.status.Socialdemokraterna}</p>
+          <p>Miljöpartiet {this.props.status.Miljöpartiet}</p>
+          <p>Centerpartiet {this.props.status.Centerpartiet}</p>
+          <p>Sverigedemokraterna {this.props.status.Sverigedemokraterna}</p>
+          <p>Moderaterna {this.props.status.Moderaterna}</p>
+          <p>Kristdemokraterna {this.props.status.Kristdemokraterna}</p>
+          <p>Liberalerna {this.props.status.Liberalerna}</p>
           <BarChart
-            width={500}
+            width={400}
             height={500}
-            data={this.dataForChart()}
-            layout="vertical" >
-            <XAxis type="number" />
-            <YAxis type="category" ticks={[0, 1, 2, 3, 4, 5]} dataKey="name" />
+            data={this.dataForChart()} >
+            <XAxis type="category" dataKey="name" />
+            <YAxis type="number" ticks={[0, 1, 2, 3]} />
             <Bar dataKey="score" fill="#82ca9d" />
             <CartesianGrid strokeDasharray="3 3" />
             <Tooltip />
