a49dba8a9b56f2f69d0077df252f97be4ce1b0ed
Sofia Boström <sofia.bostrom@aftonbladet.se>
2018-02-01 12:39:17 +0100

    CSS fixes

--- frontend/src/components/allPartiesView/index.js
+++ frontend/src/components/allPartiesView/index.js
@@ -14,43 +14,33 @@ export default class AllPartiesView extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="content">
         <div className="parties-container">
-          <h1>Här kan du läsa mer om vad alla sju riksdagspartierna tycker</h1>
-          <div className="partybox">
-            <img src={Clogo} alt="C-logga"id="logo" />
-            <a href="http://www.centerpartiet.se" id="partylink"><h2>Centerpartiet</h2></a>
-          </div>
-          <div className="partybox">
-            <img src={Kdlogo} alt="KD-logga" id="logo" />
-            <a href="http://www.kristdemokraterna.se" id="partylink"><h2>Kristdemokraterna</h2></a>
-          </div>
-          <div className="partybox">
-            <img src={Llogo} alt="L-logga" id="logo" />
-            <a href="http://www.liberalerna.se" id="partylink"><h2>Liberalerna</h2></a>
-          </div>
-          <div className="partybox">
-            <img src={Mlogo} alt="M-logga" id="logo" />
-            <a href="http://www.moderaterna.se" id="partylink"><h2>Moderaterna</h2></a>
-          </div>
-          <div className="partybox">
-
-            <img src={Mplogo} alt="Mp-logga" id="logo" />
-            <a href="http://www.miljopartiet.se" id="partylink"><h2>Miljöpartiet</h2></a>
-          </div>
-          <div className="partybox">
-            <img src={Slogo} alt="S-logga" id="logo" />
-            <a href="http://www.socialdemokraterna.se" id="partylink"><h2>Socialdemokraterna</h2></a>
-          </div>
-          <div className="partybox">
-
-            <img src={Sdlogo} alt="SD-logga" id="logo" />
-            <a href="http://www.sverigedemokraterna.se" id="partylink"><h2>Sverigedemokraterna</h2></a>
-          </div>
-          <div className="partybox">
-            <img src={Vlogo} alt="V-logga" id="logo" />
-            <a href="http://www.vansterpartiet.se" id="partylink"><h2>Vänsterpartiet</h2></a>
-          </div>
+          <h3>Här kan du läsa mer om vad alla sju riksdags-partierna tycker</h3>
+          <h6><img src={Clogo} alt="C-logga"id="logo" />
+            <a href="http://www.centerpartiet.se" id="partylink">Centerpartiet</a>
+          </h6><br />
+          <h6><img src={Kdlogo} alt="KD-logga" id="logo" />
+            <a href="http://www.kristdemokraterna.se" id="partylink">Kristdemokraterna</a>
+          </h6><br />
+          <h6><img src={Llogo} alt="L-logga" id="logo" />
+            <a href="http://www.liberalerna.se" id="partylink">Liberalerna</a>
+          </h6><br />
+          <h6><img src={Mlogo} alt="M-logga" id="logo" />
+            <a href="http://www.moderaterna.se" id="partylink">Moderaterna</a>
+          </h6><br />
+          <h6><img src={Mplogo} alt="Mp-logga" id="logo" />
+            <a href="http://www.miljopartiet.se" id="partylink">Miljöpartiet</a>
+          </h6><br />
+          <h6><img src={Slogo} alt="S-logga" id="logo" />
+            <a href="http://www.socialdemokraterna.se" id="partylink">Socialdemokraterna</a>
+          </h6><br />
+          <h6><img src={Sdlogo} alt="SD-logga" id="logo" />
+            <a href="http://www.sverigedemokraterna.se" id="partylink">Sverigedemokraterna</a>
+          </h6><br />
+          <h6><img src={Vlogo} alt="V-logga" id="logo" />
+            <a href="http://www.vansterpartiet.se" id="partylink">Vänsterpartiet</a>
+          </h6>
         </div>
       </div>
     )
